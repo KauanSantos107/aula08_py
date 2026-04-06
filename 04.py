99d52b8ef2ea9ab99e4d552bcc127c777de596f6
KauanSantos <kauangoll62@gmail.com>
2026-04-06 13:49:33 -0300

    Update 04.py

--- 04.py
+++ 04.py
@@ -1,3 +1,7 @@
+'''Escreva um programa que converta um intervalo de tempo dado em minutos,
+em horas, minutos e segundos. Por exemplo, se o tempo dado for 145,87
+minutos, o programa deve fornecer 2 h 25 min 52,2 s.'''
+
 minutosTotais = float(input("Informe os minutos: "))
 
 horas = int(minutosTotais // 60)
