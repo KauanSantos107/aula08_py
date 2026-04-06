32c7f026858c03e04b13e5db20d22e4c93188df3
KauanSantos <kauangoll62@gmail.com>
2026-04-06 13:49:10 -0300

    Update 03.py

--- 03.py
+++ 03.py
@@ -1,3 +1,9 @@
+'''Crie um algoritmo que leia quatro valores digitados pelo usuário: n, a, b, c.
+a) Se n = 1 imprimir os três valores a, b, c em ordem crescente.
+b) Se n = 2 escrever os três valores a, b, c em ordem decrescente.
+c) Se n = 3 escrever os três valores a, b, c de forma que o maior fique no
+meio'''
+
 n = int(input("Valor n ( 1, 2 ou 3) "))
 a = float(input("Valor A: "))
 b = float(input("Valor B: "))
